607fa6d3aec56f3fb7567d5c30ab963004ec64c8
unknown <spreetha15598@gmail.com>
2021-04-22 14:11:53 +0530

    first commit

--- README.cs
+++ README.cs
@@ -1,1 +1,39 @@
-# Preetha
+//arithmetic operations
+
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a=5, b=5;
+        Console.WriteLine(a+b);
+        Console.WriteLine(a-b);
+        Console.WriteLine(a*b);
+        Console.WriteLine(a/b);
+        
+    }
+}
+
+//string fuctions 
+
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name="preetha";
+        int age=22;
+        char gender='F';
+        float percentage= 75.55F;
+        
+        Console.WriteLine("name:{0}",name);
+        Console.WriteLine("gender:{0}",gender);
+        Console.WriteLine("percentage:{0}",percentage);
+    
+        
+    }
+}
